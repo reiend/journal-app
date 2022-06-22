a9997f5dd4c41b6607875e00658e277cd3d4a074
reiend <loncerasjakerei@gmail.com>
2022-06-22 22:20:34 +0800

    specs for category controller and request

---
+++ spec/requests/tasks/category_spec.rb
@@ -0,0 +1,86 @@
+require 'rails_helper'
+
+RSpec.describe 'Categories', type: :request do
+  describe 'Get' do
+    describe 'index' do
+      before(:each) do
+        get '/categories'
+      end
+      it '1, should have a successful response' do
+        expect(response).to have_http_status(200)
+      end
+      it '2, render template index' do
+        expect(response).to render_template(:index)
+      end
+    end
+
+    describe 'new' do
+      before(:each) do
+        get '/category/new'
+      end
+      it '1, should have a successful response' do
+        expect(response).to have_http_status(200)
+      end
+      it '2, render template index' do
+        expect(response).to render_template(:new)
+      end
+    end
+
+    describe 'edit' do
+      before(:each) do
+        category = Category.create(name: 'category')
+
+        get "/category/#{category.id}/edit"
+      end
+      it '1, should have a successful response' do
+        expect(response).to have_http_status(200)
+      end
+      it '2, render template edit' do
+        expect(response).to render_template(:edit)
+      end
+    end
+  end
+
+  describe 'Post' do
+    describe 'create' do
+      before(:each) do
+        post '/category/create', params: {
+          category: {
+            name: 'category'
+          }
+        }
+      end
+      it '1, should have a redirect response' do
+        expect(response).to have_http_status(302)
+      end
+    end
+  end
+
+  describe 'Patch' do
+    describe 'update' do
+      before(:each) do
+        category = Category.create(name: 'category')
+        patch "/category/#{category.id}", params: {
+          category: {
+            name: 'category'
+          }
+        }
+      end
+      it '1, should have a redirect response' do
+        expect(response).to have_http_status(302)
+      end
+    end
+  end
+
+  describe 'Delete' do
+    describe 'destroy' do
+      before(:each) do
+        category = Category.create(name: 'category')
+        delete "/category/#{category.id}"
+      end
+      it '1, should have a redirect response' do
+        expect(response).to have_http_status(302)
+      end
+    end
+  end
+end
